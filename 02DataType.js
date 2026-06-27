f7f512d8351785fe3db78d6bf57979cef967f203
Md Kaif <kaifmd9006@gmail.com>
2026-06-27 18:10:46 +0530

    JS Data Types

---
+++ 02DataType.js
@@ -0,0 +1,12 @@
+"use strict";  // treat all js code as newer version of js.
+
+let name = "Kaif"; // String Data Type
+let age = 22; // Number Data Type
+let isStudent = true;  // Boolean Data Type
+let state // Undefinded Data Type
+let data = null;  // Null (OBJECT) Data Type
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
