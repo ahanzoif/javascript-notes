c8c9a82a9bdbdbf7689a7448df799c9186c144cb
Md Kaif <kaifmd9006@gmail.com>
2026-07-11 17:53:19 +0530

    Data Type Conversion

--- 03DTConversion.js
+++ 03DTConversion.js
@@ -1,8 +1,9 @@
-let score = "33";   //My Value in form of string
+let score = "33";   // Store the value "33" as a string.
 
 // console.log(typeof score);   // Check the data type
-// console.log(typeof(score));  // Another method of check data type
+// console.log(typeof(score));  // Another way to check data type
 
-let valueInNumer = Number(score);   // Convert String into Number
-console.log(typeof valueInNumer);   // Check the data type
-console.log(valueInNumer);          // This method verify whether the value is convert or not
+let valueInNumer = Number(score);   // Convert String into a Number
+
+console.log(typeof valueInNumer);   // Verify the data type after conversion
+console.log(valueInNumer);          // Display the converted value
