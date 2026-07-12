6e62142a139175f4d4dd392e8f832bb7e1fc4f89
Md Kaif <kaifmd9006@gmail.com>
2026-07-13 00:22:58 +0530

    JS Operations Notes with example

--- 04Operations.js
+++ 04Operations.js
@@ -1,15 +1,20 @@
 // Operations
 
-// let value = 3
-// let negValue = -value
-// console.log(negValue);
+let value = 3;
+let negValue = -value; // Unary minus (-) reverses the sign of a number.(sign reverse kr deta hai)
+console.log(negValue);
 
-let value = -3
-let posValue = +value
-console.log(posValue)
+let fox = -3;
+let posFox = +fox; // Unary plus (+) converts a value to a Number. like ("33" -> 33, string -> number)
+console.log(posFox); // If the value is already a number, it does nothing.
 
-let str1 = "Hello "
-let str2 = "Kaif"
-let str3 = str1 + str2
-console.log(str3)
+// To get the absolute (always positive) value, use Math.abs().
+// abs = Absolute Value
 
+let thing = -9;
+console.log(Math.abs(thing));
+
+let str1 = "Hello ";
+let str2 = "Kaif";
+let str3 = str1 + str2; // + acts as a concatenation operator for strings.
+console.log(str3);
