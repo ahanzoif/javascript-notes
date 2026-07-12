dd19fba82fc119db4652c62938a7a3674695da54
Md Kaif <kaifmd9006@gmail.com>
2026-07-12 23:52:50 +0530

    Practice

--- 04Operations.js
+++ 04Operations.js
@@ -1,5 +1,15 @@
 // Operations
 
-let value = 3
-let negValue = -value
-console.log(negValue);
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let value = -3
+let posValue = +value
+console.log(posValue)
+
+let str1 = "Hello "
+let str2 = "Kaif"
+let str3 = str1 + str2
+console.log(str3)
+
