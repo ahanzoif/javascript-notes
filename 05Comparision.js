e8e99941996d22f30387219be983e2152d91644c
Md Kaif <kaifmd9006@gmail.com>
2026-07-14 21:41:01 +0530

    Comparision and Equality in JS

---
+++ 05Comparision.js
@@ -0,0 +1,25 @@
+console.log(null > 0)
+console.log(null == 0)
+console.log(null == undefined)
+console.log(null >= 0)
+
+// In JS Comparision and Equality both are different 
+// Comparison operators (>, <, >=, <=) → convert null to 0. (Number)
+// Loose equality (==) → null is only equal to undefined.
+// if null == undefined -> true otherwise false
+
+
+// === -> strict check 
+// This operator strictly check the data type
+
+console.log("2" === 2)
+
+
+
+
+// ******************** Note ********************
+
+
+// JS is Dynamically typed language.
+// JS determines the type at runtime.
+// The type of variable can change.
