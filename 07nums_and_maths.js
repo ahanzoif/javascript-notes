bca85075b0b4dbc394feac74d8c79f363c1ee70d
Md Kaif <kaifmd9006@gmail.com>
2026-07-19 23:39:01 +0530

    Math.max

--- 07nums_and_maths.js
+++ 07nums_and_maths.js
@@ -59,3 +59,4 @@ console.log(Math.round(4.3))  // Math.round conver values in round figure
 console.log(Math.ceil(4.2))  // Math.ceil is use to get higher round value
 console.log(Math.floor(4.9)) // Math.floor is use to get lower round value
 console.log(Math.min(4,3,5,2,0,1)) // Math.min is use to find min value
+console.log(Math.max(7,2,45,78,9475,47446)) // Math.max is use to find max value
