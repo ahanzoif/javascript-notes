23278ad6eb546172b4b4c78584aa475d0e77d3ab
Md Kaif <kaifmd9006@gmail.com>
2026-07-20 20:07:32 +0530

    toJASON

--- 08date.js
+++ 08date.js
@@ -1,7 +1,9 @@
 // ********************** Date **********************
 
 let myDate = new Date()
-console.log(myDate)
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+console.log(myDate.toJSON)
+
