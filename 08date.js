a660414d13b69834b614c83631e2524453174fd1
Md Kaif <kaifmd9006@gmail.com>
2026-07-20 20:10:39 +0530

    Local Time String

--- 08date.js
+++ 08date.js
@@ -6,5 +6,6 @@ let myDate = new Date()
 // console.log(myDate.toDateString())
 // console.log(myDate.toISOString())
 // console.log(myDate.toJSON)
-console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleTimeString())
 
