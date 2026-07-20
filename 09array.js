1a4916692a586443296af52d00e45d6337be9ada
Md Kaif <kaifmd9006@gmail.com>
2026-07-21 00:51:40 +0530

    Array Methods

--- 09array.js
+++ 09array.js
@@ -1,6 +1,26 @@
 const myArray = [0, 2, 4, 5, 6, 8]
 console.log(myArray[3])
+console.log(typeof myArray)
 
 const marvel = ["Iron Man", "Doctor Doom"]
 console.log(marvel)
+console.log(typeof marvel)
 
+const array1 = new Array(1, 3, 5, 6)
+console.log(array1)
+console.log(typeof array1)
+
+
+//********************* Array Methods *********************
+
+// myArray.push(9)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(3)
+// myArray.shift()
+
+console.log(myArray.includes(2))
+//.includes tells value is true or false on the basics of original value
+console.log(myArray.indexOf(2))
+console.log(myArray)
