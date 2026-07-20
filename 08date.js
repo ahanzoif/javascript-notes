e343e230f096ea8fb33ead01d02194157100a9e9
Md Kaif <kaifmd9006@gmail.com>
2026-07-20 20:12:39 +0530

    Date and Time

--- 08date.js
+++ 08date.js
@@ -1,11 +1,11 @@
 // ********************** Date **********************
 
 let myDate = new Date()
-// console.log(myDate)
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
-// console.log(myDate.toISOString())
-// console.log(myDate.toJSON)
-// console.log(myDate.toLocaleDateString())
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON)
+console.log(myDate.toLocaleDateString())
 console.log(myDate.toLocaleTimeString())
 
