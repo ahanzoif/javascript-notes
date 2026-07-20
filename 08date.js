ad97c7897337918ec9c1f1aa90ff9395fb9ab105
Md Kaif <kaifmd9006@gmail.com>
2026-07-20 19:44:30 +0530

    date property

--- 08date.js
+++ 08date.js
@@ -1,1 +1,7 @@
 // ********************** Date **********************
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
