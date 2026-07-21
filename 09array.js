2aea8678f1890255313e3b717b91f8b7a1f88fff
Md Kaif <kaifmd9006@gmail.com>
2026-07-21 13:38:44 +0530

    Array Basics

--- 09array.js
+++ 09array.js
@@ -1,26 +1,30 @@
 const myArray = [0, 2, 4, 5, 6, 8]
-console.log(myArray[3])
-console.log(typeof myArray)
+// console.log(myArray[3])
+// console.log(typeof myArray)
 
-const marvel = ["Iron Man", "Doctor Doom"]
-console.log(marvel)
-console.log(typeof marvel)
+// const marvel = ["Iron Man", "Doctor Doom"]
+// console.log(marvel)
+// console.log(typeof marvel)
 
-const array1 = new Array(1, 3, 5, 6)
-console.log(array1)
-console.log(typeof array1)
+// const array1 = new Array(1, 3, 5, 6)
+// console.log(array1)
+// console.log(typeof array1)
 
 
-//********************* Array Methods *********************
+// //********************* Array Methods *********************
 
-// myArray.push(9)
-// myArray.push(7)
-// myArray.pop()
+// // myArray.push(9)
+// // myArray.push(7)
+// // myArray.pop()
 
-// myArray.unshift(3)
-// myArray.shift()
+// // myArray.unshift(3)
+// // myArray.shift()
 
-console.log(myArray.includes(2))
-//.includes tells value is true or false on the basics of original value
-console.log(myArray.indexOf(2))
-console.log(myArray)
+// console.log(myArray.includes(2))
+// //.includes tells value is true or false on the basics of original value
+// console.log(myArray.indexOf(2))
+// console.log(myArray)
+
+const newArray = myArray.join()
+console.log(typeof newArray)
+console.log(newArray)
