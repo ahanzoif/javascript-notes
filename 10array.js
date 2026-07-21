1a8d77c7eb18347bf8f8bd9f35336c4c2ebfacc8
Md Kaif <kaifmd9006@gmail.com>
2026-07-21 15:25:04 +0530

    .flat operator

--- 10array.js
+++ 10array.js
@@ -5,13 +5,22 @@ const dc = ["Batman", "Superman", "Wonder Woman"]
 // console.log(marvel)  //write the good and readable form of code
 // console.log(marvel[3][2])
 
-const comics = marvel.concat(dc)
+const comics = marvel.concat(dc)    // concat operator
 //.conca -> Combines two or more arrays. 
 // This method returns a new array without modifying any existing arrays.
 // console.log(comics)
 
-const heros = [...marvel, ...dc]
+const heros = [...marvel, ...dc]    // spread operator
 console.log(heros)
 //Spread operator(...), this operator is one of the most
 //commonly used features in moder JS, insted of concat operator
 //The ... takes each element out of the array and places it into the new array.
+
+const nums = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const Allnums = nums.flat(Infinity)
+console.log(Allnums)
+//.flat -> Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+//Infinity -> The maximum recursion depth.
+//Depth -> Arrays in Array.
+
+
