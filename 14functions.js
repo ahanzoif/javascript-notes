813d72485595e035a341571332d9fab874f8a1d5
Md Kaif <kaifmd9006@gmail.com>
2026-07-25 17:52:26 +0530

    Function Methods

--- 14functions.js
+++ 14functions.js
@@ -1,15 +1,28 @@
-function name (){
-    console.log("K");
-    console.log("A");
-    console.log("I");
-    console.log("F"); 
+function name() {
+  console.log("K");
+  console.log("A");
+  console.log("I");
+  console.log("F");
 }
-name()
+name();
 
-function sumOftwo(num1, num2)
-{
- let result = num1 + num2
- return result
+// *******************************************
+
+function sumOftwo(num1, num2) {
+  let result = num1 + num2;
+  return result;
 }
-const result = sumOftwo(4, 6)
+const result = sumOftwo(4, 6);
 console.log("Result is: ", result);
+
+// *******************************************
+
+function userMsg(userName) {
+  if (!userName) {
+    console.log("Please enter a valid username");
+    return;
+  }
+  return `${userName} just logged in`;
+}
+console.log(userMsg("Md Kaif"));
+console.log(userMsg(""));
