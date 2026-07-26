bbb116020ca068f44cd15a358b8f4562ae6863ce
Md Kaif <kaifmd9006@gmail.com>
2026-07-26 17:42:42 +0530

    Local & Global Scope

--- 15scope.js
+++ 15scope.js
@@ -1,19 +1,23 @@
 // *********************Local & Global Scope *********************
 
 // var c = 300
-let a = 300;
+// let a = 300;
 const b = 60;
 
 if (true) {
   let a = 10;
   const b = 20;
   // var c = 40
-  console.log("Inner a: ", a);
+  console.log(
+    "Hey, I'm Local Scope",
+    b,
+    "I only use in my block, out of my block you never access me",
+  );
 }
 
-console.log("Outer a: ", a);
+// console.log("Outer a: ", a);
 console.log(
-  "Hey there! ",
+  "Hey there!",
   "I'm",
   b,
   "and I'm Global Scope, we can use me any block of code.",
