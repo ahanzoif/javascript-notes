32061fe060fbb9210e347de54b5c130bd87d85b3
Md Kaif <kaifmd9006@gmail.com>
2026-07-26 19:09:33 +0530

    Closure Scope

--- 16scoope.js
+++ 16scoope.js
@@ -7,5 +7,17 @@ function one() {
   }
   two(); // twp() -> call the function two() to be execute
 }
-one(); // one() -> call the function one() to be execute
+// one(); // one() -> call the function one() to be execute
 // if we not call function one the code will not give any output
+
+// *******************************************
+
+if (true) {
+  const username = "kaif";
+  if (username === "kaif") {
+    const website = " bytebuster";
+    console.log(username + website);
+  }
+  //   console.log(website);  -> gives us error because website exist in local scope and we access it out of the their block
+}
+// console.log(username); -> it also give the error because of the scope
