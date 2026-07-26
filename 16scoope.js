21289e12602a146cba7adbc630f30b1806896538
Md Kaif <kaifmd9006@gmail.com>
2026-07-26 18:27:31 +0530

    Closure scope

---
+++ 16scoope.js
@@ -0,0 +1,11 @@
+function one() {
+  const username = "kaif";
+
+  function two() {
+    const website = "bytebuster";
+    console.log(username);
+  }
+  two(); // twp() -> call the function two() to be execute
+}
+one(); // one() -> call the function one() to be execute
+// if we not call function one the code will not give any output
