8499e4e685ee7671138a1d1f27998e8ea89b70a2
Md Kaif <kaifmd9006@gmail.com>
2026-08-05 14:06:38 +0530

    Arrow function

--- 17arrowfunction.js
+++ 17arrowfunction.js
@@ -11,3 +11,24 @@ user.welcomeMessage()
 user.username = "Ahan"
 user.welcomeMessage()
 console.log(this);
+// it gives empty{} because there is no global variable to refer or context of the -this-
+
+// *********************** Notes ***********************
+
+// -this.- refers to the context of the user
+
+// In function (this) is Dynamic but in Arrow function (this) is Lexical
+
+// You have to define Global variable to use (this) in Arrow Function
+
+function check () {
+    let username = "Ashiya"
+    console.log(this.username);
+}
+check()
+// this works in object not in a function
+
+const syntax = () => {
+    let name = "kaif"
+    console.log(name);
+}
