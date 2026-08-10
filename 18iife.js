e9bcdc405374c4fba60bdc1f47b0ec72ea2c7fc0
Md Kaif <kaifmd9006@gmail.com>
2026-08-10 23:26:03 +0530

    IIFE

--- 18iife.js
+++ 18iife.js
@@ -1,12 +1,36 @@
-// const x = 20;
-// (function () {
-//   let x = 10;
-//   console.log(x);
-// })();
-// console.log(x);
+// IIFE -> Immediately Invoked Function Expressions
+
+const x = 20;
+(function () {
+  let x = 10;
+  console.log(x);
+})();
+console.log(x);
 
 (function () {
     let hp = 20;
 })();
 
-let result = hidden;
+// let result = hidden;
+
+
+// IIFE is a function that is created and executed immediately. 
+// IIFE is used to execute code immediately.
+
+// first () is function expressions 
+// second () is use to immediately invokes it.
+
+
+(function nerd(){
+    console.log(`named IIFE`);
+})();
+
+( () => {
+    console.log(`un-named IIFE`);
+})();
+
+( (silver) => {
+    console.log(`parameters pass, ${silver}`);
+})(`IIFE`)
+
+// Always add semicolumn to end Invoked
