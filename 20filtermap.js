cc027bb29a57f0c6dc161948fe8c90ec104bca92
Md Kaif <kaifmd9006@gmail.com>
2026-08-16 23:06:26 +0530

    filter

--- 20filtermap.js
+++ 20filtermap.js
@@ -3,12 +3,24 @@
 // const newNums = myNums.filter( (num) => num > 4)
 // console.log(newNums);
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newNums = myNums.filter((num) => {
-  return num > 4;
-});
+// const newNums = myNums.filter((num) => {
+//   return num > 4;
+// });
 
-console.log(newNums);
+// console.log(newNums);
 
 //both code are same method are different in curly braces.
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
